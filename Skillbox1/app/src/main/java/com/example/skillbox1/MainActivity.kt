a1e8a96953872c2fcf67d1998bafa1d0844f7bc1
DESKTOP-9QFFJC9\gavri <gavrikov.sergey@gmail.com>
2020-12-10 22:49:32 +0300

    no message

--- Skillbox1/app/src/main/java/com/example/skillbox1/MainActivity.kt
+++ Skillbox1/app/src/main/java/com/example/skillbox1/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.skillbox1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button1).setOnClickListener {
+            //println("Нажата кнопка 1")
+            Log.e("!@#","bottom 1 pressed")
+        }
+        findViewById<Button>(R.id.button2).setOnClickListener {
+            //println("Нажата кнопка 2")
+            Log.e("!@#","bottom 2 pressed")
+        }
+        findViewById<Button>(R.id.button3).setOnClickListener {
+            //println("Нажата кнопка 3")
+            Log.e("!@#","bottom 3 pressed")
+        }
+
     }
 }
